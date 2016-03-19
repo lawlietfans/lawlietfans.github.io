aac20cb612aa3078eb9bbb2a57482140c5bbff5d
lawlietfans <fengshenjie.email@qq.com>
2016-03-19 10:46:49 +0800

    v1-over

--- deploycoding.sh
+++ deploycoding.sh
@@ -4,6 +4,10 @@ cp -r public/* ../lawlietfans/
 
 #push
 cd ../lawlietfans
+#$ DATE=$(date +%Y-%m-%d)
+#$ echo $DATE 
+#2016-03-13
+DATE=$(date)
 git add --all
-git commit -m 'autodeploy'
+git commit -m "$DATE autodeploy"
 git push origin master
